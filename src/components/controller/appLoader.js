88628388902186c7abec33f2482a0feef9fc97f8
balaxon <balahanov2209@gmail.com>
2022-12-07 17:29:46 +0300

    init: start task

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '853f1c001efb4bb590ba6be9b7c65203', // получите свой ключ https://newsapi.org/
         });
     }
 }
