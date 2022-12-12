93a3a410b0283b556c12897a8ef90fb053d26efc
balaxon <balahanov2209@gmail.com>
2022-12-12 13:04:46 +0300

    fix: proxy

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.up.railway.app/', {
             apiKey: '853f1c001efb4bb590ba6be9b7c65203', // получите свой ключ https://newsapi.org/
         });
     }
